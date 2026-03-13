3e25182bbde7b2a9fdba6ca229feb340c85c13f5
diegobreeg <diegobreeg@gmail.com>
2026-03-12 23:06:44 -0300

    Remove auto redirect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,8 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
-        //
+        $middleware->redirectGuestsTo(fn () => null);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            if ($request->expectsJson() || $request->is('api/*')) {
+                return response()->json(['message' => 'Unauthenticated.'], 401);
+            }
+        });
     })->create();
